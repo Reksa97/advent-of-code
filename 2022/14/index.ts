16807c0e84fbd4e67e23b3310119eeb2d9e44595
Reko <reko.kalkaja@gmail.com>
2022-12-16 00:35:51 +0200

    2022 day 14 solved

--- 2022/14/index.ts
+++ 2022/14/index.ts
@@ -16,12 +16,12 @@ const sandStartPosition: Position = {
 const rockPositions = new Set<string>()
 const input = readFileSync('./14/input.txt').toString().split(/\n/)
 
-let minX = Infinity
-let maxX = -Infinity
-let minY = Infinity
-let maxY = -Infinity
+let minX = sandStartPosition.x
+let maxX = sandStartPosition.x
+let minY = sandStartPosition.y
+let maxY = sandStartPosition.y
 
-input.forEach((line, i) => {
+input.forEach((line) => {
   const rockEdges: Position[] = line.split(ARROW).map(p => {
     const [x, y] = p.split(',').map(c => parseInt(c))
     if (y > maxY) maxY = y
@@ -61,18 +61,40 @@ input.forEach((line, i) => {
     // One of two above if cases should be always true, otherwise input is malformed
   }
 })
+const floorY = maxY + 2
 maxY++
 maxX++
 minY--
 minX--
 
-const simulateSand = (startPosition: Position, rocks: Set<string>, maxY: number) => {
+const simulateSand = (startPosition: Position, rocks: Set<string>, floorY: number, part: 1 | 2) => {
   const sand = new Set<string>()
 
   const isPositionFree = (pos: Position) => {
     const posString = positionToString(pos)
     const positionIsFree = !rocks.has(posString) && !sand.has(posString)
     return positionIsFree
+  }
+
+  if (part === 2) {
+    sand.add(positionToString(startPosition))
+    let leftX = startPosition.x
+    let rightX = startPosition.x
+    for (let y = startPosition.y + 1; y < floorY; y++) {
+      leftX--
+      rightX++
+      for (let x = leftX; x <= rightX; x++) {
+        const positionString = positionToString({ x, y })
+        const positionIsFree = !rocks.has(positionString)
+        const sandComingAbove = [{ x: x - 1, y: y - 1 }, { x: x, y: y - 1 }, { x: x + 1, y: y - 1 }].map(p => positionToString(p)).some(p => sand.has(p))
+        if (positionIsFree && sandComingAbove) {
+          sand.add(positionString)
+        }
+      }
+    }
+    minX = leftX
+    maxX = rightX
+    return { sand }
   }
 
   const lastSandPath = new Set<string>()
@@ -107,31 +129,44 @@ const simulateSand = (startPosition: Position, rocks: Set<string>, maxY: number)
   }
   return { sand, lastSandPath }
 }
-const { sand, lastSandPath } = simulateSand(sandStartPosition, rockPositions, maxY)
 
-for (let y = minY; y <= maxY; y++) {
-  let row = ''
-  for (let x = minX; x <= maxX; x++) {
-    const positionString = positionToString({ x, y })
-    if (sand.has(positionString)) {
-      row += 'O'
-      continue
+const printTiles = (part: 1 | 2, sand: Set<string>, lastSandPath?: Set<string>) => {
+  for (let y = minY; y <= floorY; y++) {
+    let row = ''
+    for (let x = minX; x <= maxX; x++) {
+      const positionString = positionToString({ x, y })
+      if (positionToString(sandStartPosition) === positionString) {
+        row += sand.has(positionString) ? '🟡' : '🔻'
+        continue
+      }
+      if (sand.has(positionString)) {
+        row += '🟨'
+        continue
+      }
+      if (rockPositions.has(positionString)) {
+        row += '🟫'
+        continue
+      }
+      if (lastSandPath && lastSandPath.has(positionString)) {
+        row += '🔻'
+        continue
+      }
+      if (y === floorY && part === 2) {
+        row += '⬛️'
+        continue
+      }
+      row += '⬜️'
     }
-    if (rockPositions.has(positionString)) {
-      row += '#'
-      continue
-    }
-    if (lastSandPath.has(positionString)) {
-      row += '~'
-      continue
-    }
-    if (positionToString(sandStartPosition) === positionString) {
-      row += '+'
-      continue
-    }
-    row += '.'
+    console.log(row)
   }
-  console.log(row)
 }
 
+let { sand, lastSandPath } = simulateSand(sandStartPosition, rockPositions, floorY, 1)
+printTiles(1, sand, lastSandPath)
+
 console.log(`part 1: there are ${sand.size} units of settled sand`)
+
+const secondResult = simulateSand(sandStartPosition, rockPositions, floorY, 2)
+printTiles(2, secondResult.sand)
+
+console.log(`part 2: there are ${secondResult.sand.size} units of settled sand`)
