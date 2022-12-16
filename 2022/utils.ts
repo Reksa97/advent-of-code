196262483d2eec05192ec1b10b180835150f1186
Reko <reko.kalkaja@gmail.com>
2022-12-16 20:44:05 +0200

    2022 day 16 part solved

--- 2022/utils.ts
+++ 2022/utils.ts
@@ -7,3 +7,21 @@ export interface Position {
 export const positionToString = (position: Position): string => {
   return `${position.x}:${position.y}`
 }
+
+export const permutator = <T>(inputArr: T[]): T[][] => {
+  let result = [];
+
+  const permute = (arr: T[], m = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+      }
+    }
+  }
+  permute(inputArr)
+  return result
+}
