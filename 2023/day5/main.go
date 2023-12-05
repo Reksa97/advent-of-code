a154a93778baa8ded7383fda3eb32cfafcd3775e
Reksa97 <reko.kalkaja@gmail.com>
2023-12-05 14:46:32 +0200

    2023 solve day 5 part 2

--- 2023/day5/main.go
+++ 2023/day5/main.go
@@ -52,22 +52,15 @@ func toMapName(s string) MapName {
 	return None
 }
 
-/* type MapInt int
-
-const (
-	SeedToSoil MapInt = iota
-	SoilToFertilizer
-	FertilizerToWater
-	WaterToLight
-	LightToTemperature
-	TemperatureToHumidity
-	HumidityToLocation
-) */
-
 type Map struct {
 	destination int
 	source      int
 	size        int
+}
+
+type SourceRange struct {
+	min int
+	max int
 }
 
 func findDestination(maps map[MapName][]Map, mapName MapName, source int) int {
@@ -79,13 +72,63 @@ func findDestination(maps map[MapName][]Map, mapName MapName, source int) int {
 	return source
 }
 
-func partOne(lines []string, debug bool) {
-	seeds := strings.Split(strings.Split(lines[0], "seeds: ")[1], " ")
+func findDestinations(maps map[MapName][]Map, mapName MapName, sources SourceRange) []SourceRange {
+	destinationRanges := []SourceRange{}
+	for _, m := range maps[mapName] {
+		if sources.min >= m.source && sources.max < m.source+m.size {
+			// whole source range goes in single destination range, no need to split source range
+			destinationRanges = append(destinationRanges, SourceRange{
+				min: m.destination + (sources.min - m.source),
+				max: m.destination + (sources.max - m.source),
+			})
+			return destinationRanges
+		} else if sources.min >= m.source && sources.min < m.source+m.size {
+			// source range starts in destination range but goes over it
+			destinationRanges = append(destinationRanges, SourceRange{
+				min: m.destination + (sources.min - m.source),
+				max: m.destination + (m.size - 1),
+			})
+			destinationRanges = append(destinationRanges, findDestinations(maps, mapName, SourceRange{
+				min: m.source + m.size,
+				max: sources.max,
+			})...)
+			return destinationRanges
+		} else if sources.max >= m.source && sources.max < m.source+m.size {
+			// source range ends in destination range but starts before it
+			destinationRanges = append(destinationRanges, SourceRange{
+				min: m.destination,
+				max: m.destination + (sources.max - m.source),
+			})
+			destinationRanges = append(destinationRanges, findDestinations(maps, mapName, SourceRange{
+				min: sources.min,
+				max: m.source - 1,
+			})...)
+			return destinationRanges
+		} else if sources.min < m.source && sources.max >= m.source+m.size {
+			// source range starts before destination range and ends after it
+			destinationRanges = append(destinationRanges, SourceRange{
+				min: m.destination,
+				max: m.destination + (m.size - 1),
+			})
+			destinationRanges = append(destinationRanges, findDestinations(maps, mapName, SourceRange{
+				min: sources.min,
+				max: m.source - 1,
+			})...)
+			destinationRanges = append(destinationRanges, findDestinations(maps, mapName, SourceRange{
+				min: m.source + m.size,
+				max: sources.max,
+			})...)
+			return destinationRanges
+		}
+	}
+	// no destination range found, return original source range
+	destinationRanges = append(destinationRanges, sources)
+	return destinationRanges
+}
 
-	mapName := None
-
+func getMaps(lines []string, debug bool) map[MapName][]Map {
 	maps := make(map[MapName][]Map)
-
+	mapName := None
 	for _, line := range lines[2:] {
 		if mapName == "" && strings.HasSuffix(line, " map:") {
 			mapName = toMapName(strings.Split(line, " map:")[0])
@@ -109,13 +152,18 @@ func partOne(lines []string, debug bool) {
 			source:      source,
 			size:        size,
 		})
-
 	}
 
 	if debug {
 		fmt.Println(maps)
 	}
+	return maps
+}
 
+func partOne(lines []string, debug bool) {
+	seeds := strings.Split(strings.Split(lines[0], "seeds: ")[1], " ")
+
+	maps := getMaps(lines, debug)
 	minLocation := math.MaxInt
 	for _, seedString := range seeds {
 		source, _ := strconv.Atoi(seedString)
@@ -140,7 +188,34 @@ func partOne(lines []string, debug bool) {
 }
 
 func partTwo(lines []string, debug bool) {
-
+	seeds := strings.Split(strings.Split(lines[0], "seeds: ")[1], " ")
+	minLocation := math.MaxInt
+	maps := getMaps(lines, debug)
+	for i := 0; i < len(seeds); i += 2 {
+		min, _ := strconv.Atoi(seeds[i])
+		size, _ := strconv.Atoi(seeds[i+1])
+		sourceRange := SourceRange{min: min, max: min + size - 1}
+		sourceRanges := []SourceRange{sourceRange}
+		for _, mapName := range allMapNames {
+			newSourceRanges := []SourceRange{}
+			for _, sourceRange := range sourceRanges {
+				newSourceRanges = append(newSourceRanges, findDestinations(maps, mapName, sourceRange)...)
+			}
+			if debug {
+				fmt.Println(mapName, newSourceRanges)
+			}
+			sourceRanges = newSourceRanges
+		}
+		for _, sourceRange := range sourceRanges {
+			if sourceRange.min < minLocation {
+				minLocation = sourceRange.min
+			}
+		}
+		if debug {
+			fmt.Println(sourceRange)
+		}
+	}
+	fmt.Println("Lowest location number", minLocation)
 }
 
 func main() {
