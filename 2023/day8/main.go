4bec12f719283b5a55b781e7a081021ea3c94621
Reko <reko.kalkaja@gmail.com>
2023-12-08 19:07:03 +0200

    2023 solve day 8 part 2

--- 2023/day8/main.go
+++ 2023/day8/main.go
@@ -17,27 +17,34 @@ type Node struct {
 	name  string
 }
 
-func parseInput(lines []string) (string, map[string]Node) {
+func parseInput(lines []string) (string, map[string]Node, []string) {
 	instructions := lines[0]
 	nodesMap := map[string]Node{}
+	var startNodes []string
 	nodes := lines[2:]
 	for _, node := range nodes {
 		nodeParts := strings.Split(node, " ")
 		left := nodeParts[2][1 : len(nodeParts[2])-1]
 		right := nodeParts[3][0 : len(nodeParts[3])-1]
-		nodesMap[nodeParts[0]] = Node{left: left, right: right, name: nodeParts[0]}
+		name := nodeParts[0]
+		nodesMap[nodeParts[0]] = Node{left: left, right: right, name: name}
+		if name[len(name)-1] == 'Z' {
+			startNodes = append(startNodes, name)
+		}
 	}
-	return instructions, nodesMap
+	return instructions, nodesMap, startNodes
 }
 
 func partOne(lines []string) {
-	instructions, nodesMap := parseInput(lines)
+	instructions, nodesMap, _ := parseInput(lines)
 
 	currentNode := "AAA"
 
 	steps := 0
 	for i := 1; true; i++ {
-		fmt.Printf("Iteration: %v\n", i)
+		if debug {
+			fmt.Printf("Iteration: %v\n", i)
+		}
 		for _, instruction := range instructions {
 			steps++
 			if debug {
@@ -63,11 +70,118 @@ func partOne(lines []string) {
 			break
 		}
 	}
+}
 
+// Function to calculate GCD
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+// Function to calculate LCM of two numbers
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+// Function to calculate LCM of an array of integers
+func lcmArray(arr []int) int {
+	result := arr[0]
+	for i := 1; i < len(arr); i++ {
+		result = lcm(result, arr[i])
+	}
+	return result
 }
 
 func partTwo(lines []string) {
+	instructions, nodesMap, nodes := parseInput(lines)
 
+	steps := 0
+	for i := 1; true; i++ {
+		if debug {
+			fmt.Printf("Iteration: %v\n", i)
+		} else if i%1000000 == 0 {
+			fmt.Printf("Iteration: %v\n", i)
+		}
+		for _, instruction := range instructions {
+			steps++
+			for nodeIndex, node := range nodes {
+				if debug {
+					fmt.Printf("nodeIndex: %v, node: %v, instruction: %v\n", nodeIndex, node, instruction)
+				}
+				switch instruction {
+				case 'L':
+					if debug {
+						fmt.Println("left")
+					}
+					nodes[nodeIndex] = nodesMap[node].left
+				case 'R':
+					if debug {
+						fmt.Println("right")
+					}
+					nodes[nodeIndex] = nodesMap[node].right
+				default:
+					fmt.Println("error")
+				}
+			}
+		}
+
+		everythingInItsRightPlace := true
+		for _, node := range nodes {
+			if node[len(node)-1] != 'Z' {
+				everythingInItsRightPlace = false
+			}
+		}
+		if everythingInItsRightPlace {
+			fmt.Printf("Found it! Iteration: %v, steps: %v\n", i, steps)
+			break
+		}
+	}
+}
+
+func partTwoOptimised(lines []string) {
+	instructions, nodesMap, nodes := parseInput(lines)
+
+	nodeIterations := make([]int, len(nodes))
+	for nodeIndex, node := range nodes {
+		currentNode := node
+		steps := 0
+		for i := 1; true; i++ {
+			if debug {
+				fmt.Printf("Iteration: %v\n", i)
+			}
+			for _, instruction := range instructions {
+				steps++
+				if debug {
+					fmt.Printf("currentNode: %v, instruction: %v\n", currentNode, instruction)
+				}
+				switch instruction {
+				case 'L':
+					if debug {
+						fmt.Println("left")
+					}
+					currentNode = nodesMap[currentNode].left
+				case 'R':
+					if debug {
+						fmt.Println("right")
+					}
+					currentNode = nodesMap[currentNode].right
+				default:
+					fmt.Println("error")
+				}
+			}
+			if currentNode[len(currentNode)-1] == 'Z' {
+				fmt.Printf("Found it! Node %v, Iteration: %v, steps: %v, %v\n", node, i, steps, steps/i)
+				nodeIterations[nodeIndex] = i
+				break
+			}
+		}
+	}
+	fmt.Printf("nodeIterations: %v\n", nodeIterations)
+	leastCommonMultiple := lcmArray(nodeIterations)
+	fmt.Printf("LCM: %v\n", leastCommonMultiple)
+	fmt.Printf("Steps: %v\n", leastCommonMultiple*len(instructions))
 }
 
 func main() {
@@ -84,7 +198,7 @@ func main() {
 	if part == 1 {
 		partOne(lines)
 	} else {
-		partTwo(lines)
+		partTwoOptimised(lines)
 	}
 
 	endTime := time.Now()
