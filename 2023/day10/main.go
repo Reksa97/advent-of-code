974a907c3556714db3932a7945146f2610599c01
Reko <reko.kalkaja@gmail.com>
2023-12-11 11:55:22 +0200

    2023 solve day 10 part 2

--- 2023/day10/main.go
+++ 2023/day10/main.go
@@ -23,12 +23,75 @@ func parseInput(lines []string) (Position, [][]int) {
 
 		for x, char := range line {
 			distances[y] = append(distances[y], -1)
+			comingFromUp := y > 0 && (lines[y-1][x] == '|' || lines[y-1][x] == 'F' || lines[y-1][x] == '7')
+			comingFromDown := y < len(lines)-1 && (lines[y+1][x] == '|' || lines[y+1][x] == 'J' || lines[y+1][x] == 'L')
+			comingFromLeft := x > 0 && (lines[y][x-1] == '-' || lines[y][x-1] == 'L' || lines[y][x-1] == 'F')
+			comingFromRight := x < len(lines[0])-1 && (lines[y][x+1] == '-' || lines[y][x+1] == 'J' || lines[y][x+1] == '7')
 			if char == 'S' {
 				start = Position{x: x, y: y}
+				if comingFromUp && comingFromDown {
+					lines[y] = lines[y][:x] + "|" + lines[y][x+1:]
+				} else if comingFromLeft && comingFromRight {
+					lines[y] = lines[y][:x] + "-" + lines[y][x+1:]
+				} else if comingFromUp && comingFromLeft {
+					lines[y] = lines[y][:x] + "J" + lines[y][x+1:]
+				} else if comingFromUp && comingFromRight {
+					lines[y] = lines[y][:x] + "L" + lines[y][x+1:]
+				} else if comingFromDown && comingFromLeft {
+					lines[y] = lines[y][:x] + "7" + lines[y][x+1:]
+				} else if comingFromDown && comingFromRight {
+					lines[y] = lines[y][:x] + "F" + lines[y][x+1:]
+				} else {
+					panic(fmt.Sprintf("Invalid start position: %v", start))
+				}
 			}
 		}
 	}
 	return start, distances
+}
+
+func getExpandedInput(lines []string) []string {
+	expandedLines := []string{}
+	for y, line := range lines {
+		expandedLines = append(expandedLines, "", "", "")
+		for _, char := range line {
+			switch char {
+			case '.':
+				expandedLines[y*3] += "..."
+				expandedLines[y*3+1] += "..."
+				expandedLines[y*3+2] += "..."
+			case '|':
+				expandedLines[y*3] += ".|."
+				expandedLines[y*3+1] += ".|."
+				expandedLines[y*3+2] += ".|."
+			case '-':
+				expandedLines[y*3] += "..."
+				expandedLines[y*3+1] += "---"
+				expandedLines[y*3+2] += "..."
+			case 'L':
+				expandedLines[y*3] += ".|."
+				expandedLines[y*3+1] += ".L-"
+				expandedLines[y*3+2] += "..."
+			case 'J':
+				expandedLines[y*3] += ".|."
+				expandedLines[y*3+1] += "-J."
+				expandedLines[y*3+2] += "..."
+			case 'F':
+				expandedLines[y*3] += "..."
+				expandedLines[y*3+1] += ".F-"
+				expandedLines[y*3+2] += ".|."
+			case '7':
+				expandedLines[y*3] += "..."
+				expandedLines[y*3+1] += "-7."
+				expandedLines[y*3+2] += ".|."
+			case 'S':
+				expandedLines[y*3] += "..."
+				expandedLines[y*3+1] += ".S."
+				expandedLines[y*3+2] += "..."
+			}
+		}
+	}
+	return expandedLines
 }
 
 func traverse(lines []string, position Position, distances [][]int, prevDistance int) {
@@ -124,8 +187,69 @@ func partOne(lines []string) {
 	fmt.Printf("Answer: %v\n", max)
 }
 
-func partTwo(lines []string) {
+func expandOuterDots(lines []string, position Position) {
+	if position.x < 0 || position.x >= len(lines[0]) || position.y < 0 || position.y >= len(lines) {
+		return
+	}
+	char := lines[position.y][position.x]
+	if char == '.' {
+		lines[position.y] = lines[position.y][:position.x] + "0" + lines[position.y][position.x+1:]
+		expandOuterDots(lines, Position{x: position.x - 1, y: position.y})     // left
+		expandOuterDots(lines, Position{x: position.x + 1, y: position.y})     // right
+		expandOuterDots(lines, Position{x: position.x, y: position.y - 1})     // up
+		expandOuterDots(lines, Position{x: position.x, y: position.y + 1})     // down
+		expandOuterDots(lines, Position{x: position.x - 1, y: position.y - 1}) // left and up
+		expandOuterDots(lines, Position{x: position.x - 1, y: position.y + 1}) // left and down
+		expandOuterDots(lines, Position{x: position.x + 1, y: position.y - 1}) // right and up
+		expandOuterDots(lines, Position{x: position.x + 1, y: position.y + 1}) // right and down
+	}
+}
 
+func partTwo(lines []string) {
+	startPosition, distances := parseInput(lines)
+	traverse(lines, startPosition, distances, -1)
+	for y, line := range distances {
+		for x, distance := range line {
+			if distance == -1 {
+				lines[y] = lines[y][:x] + "." + lines[y][x+1:]
+			}
+		}
+	}
+	lines = getExpandedInput(lines)
+	if debug {
+		for _, line := range lines {
+			fmt.Println(line)
+		}
+	}
+	for x, char := range lines[0] {
+		if char == '.' {
+			expandOuterDots(lines, Position{x: x, y: 0})
+		}
+	}
+	for x, char := range lines[len(lines)-1] {
+		if char == '.' {
+			expandOuterDots(lines, Position{x: x, y: len(lines) - 1})
+		}
+	}
+	for y, line := range lines {
+		if line[0] == '.' {
+			expandOuterDots(lines, Position{x: 0, y: y})
+		}
+		if line[len(line)-1] == '.' {
+			expandOuterDots(lines, Position{x: len(line) - 1, y: y})
+		}
+	}
+	dotsInsideLoop := 0
+	for y := 1; y < len(lines)-1; y += 3 {
+		for x := 1; x < len(lines[0])-1; x += 3 {
+			if lines[y-1][x-1] == '.' && lines[y-1][x] == '.' && lines[y-1][x+1] == '.' &&
+				lines[y][x-1] == '.' && lines[y][x] == '.' && lines[y][x+1] == '.' &&
+				lines[y+1][x-1] == '.' && lines[y+1][x] == '.' && lines[y+1][x+1] == '.' {
+				dotsInsideLoop++
+			}
+		}
+	}
+	fmt.Printf("Answer: %v\n", dotsInsideLoop)
 }
 
 func main() {
