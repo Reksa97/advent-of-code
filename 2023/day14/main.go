ff833f45e159e49a2861e865acde52bb067efa1f
Reksa97 <reko.kalkaja@gmail.com>
2023-12-14 15:26:52 +0200

    2023 solve day 14 part 2

--- 2023/day14/main.go
+++ 2023/day14/main.go
@@ -41,6 +41,39 @@ func tilt(grid [][]string, direction string) [][]string {
 				}
 			}
 		}
+	case "south":
+		for y := len(grid) - 2; y >= 0; y-- {
+			for x := 0; x < len(grid[y]); x++ {
+				if newGrid[y][x] == "O" {
+					for yy := y + 1; yy < len(grid) && newGrid[yy][x] == "."; yy++ {
+						newGrid[yy][x] = "O"
+						newGrid[yy-1][x] = "."
+					}
+				}
+			}
+		}
+	case "west":
+		for x := 1; x < len(grid[0]); x++ {
+			for y := 0; y < len(grid); y++ {
+				if newGrid[y][x] == "O" {
+					for xx := x - 1; xx >= 0 && newGrid[y][xx] == "."; xx-- {
+						newGrid[y][xx] = "O"
+						newGrid[y][xx+1] = "."
+					}
+				}
+			}
+		}
+	case "east":
+		for x := len(grid[0]) - 2; x >= 0; x-- {
+			for y := 0; y < len(grid); y++ {
+				if newGrid[y][x] == "O" {
+					for xx := x + 1; xx < len(grid[0]) && newGrid[y][xx] == "."; xx++ {
+						newGrid[y][xx] = "O"
+						newGrid[y][xx-1] = "."
+					}
+				}
+			}
+		}
 	default:
 		panic("Unknown direction")
 	}
@@ -74,8 +107,57 @@ func partOne(lines []string) {
 	fmt.Printf("Full load: %v\n", fullLoad)
 }
 
-func partTwo(lines []string) {
+type Cycle struct {
+	i     int
+	cycle int
+}
 
+func partTwo(lines []string) {
+	cycles := 1000000000
+	grid := parseInput(lines)
+	differentGrids := make(map[string]Cycle)
+	for i := 1; i <= cycles; i++ {
+		if i%(cycles/100) == 0 {
+			fmt.Println((i*100)/cycles, "%")
+		}
+		grid = tilt(grid, "north")
+		grid = tilt(grid, "west")
+		grid = tilt(grid, "south")
+		grid = tilt(grid, "east")
+		gridString := ""
+		for _, line := range grid {
+			gridString += fmt.Sprintf("%v", line)
+		}
+		if cycle, ok := differentGrids[gridString]; ok {
+			if debug {
+				fmt.Println("Found repeating grid at", i, cycle)
+			}
+			if i-cycle.i == cycle.cycle {
+				if (cycles-i)%cycle.cycle == 0 {
+					if debug {
+						fmt.Println("Found cycle that end at the end of all cycles", cycle.cycle)
+					}
+					break
+				}
+			}
+			differentGrids[gridString] = Cycle{i, i - cycle.i}
+		} else {
+			differentGrids[gridString] = Cycle{i, -1}
+		}
+	}
+	fullLoad := 0
+	for y, line := range grid {
+		if debug {
+			fmt.Println(line)
+		}
+
+		for _, char := range line {
+			if char == "O" {
+				fullLoad += len(grid) - y
+			}
+		}
+	}
+	fmt.Printf("Full load: %v\n", fullLoad)
 }
 
 func main() {
