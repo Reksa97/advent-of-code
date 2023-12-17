520f9159d8e2e7ad4be5bd49590ee55faa729b10
Reko <reko.kalkaja@gmail.com>
2023-12-17 23:17:33 +0200

    2023 solve day 17 part 2

--- 2023/day17/main.go
+++ 2023/day17/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -94,93 +95,190 @@ func findPathWithLeastHeatLoss(pq *PriorityQueue, lines []string, smallestHeatLo
 
 	smallestHeatLoss[fromY][fromX][key] = heatLossOnPath
 
-	if fromX == toX && fromY == toY {
+	if part == 1 && fromX == toX && fromY == toY {
+		return heatLossOnPath
+	}
+	if part == 2 && fromX == toX && fromY == toY && straightDistance >= 4 {
 		return heatLossOnPath
 	}
 
-	switch direction {
-	case "right":
-		// try to go down
-		heap.Push(pq, &Item{
-			value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: 1},
-			priority: heatLossOnPath,
-		})
-		// try to go up
-		heap.Push(pq, &Item{
-			value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: 1},
-			priority: heatLossOnPath,
-		})
-		if straightDistance < 3 {
-			// continue straight
+	if part == 1 {
+		switch direction {
+		case "right":
+			// try to go down
 			heap.Push(pq, &Item{
-				value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+				value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: 1},
 				priority: heatLossOnPath,
 			})
-		}
-	case "left":
-		// try to go down
-		heap.Push(pq, &Item{
-			value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: 1},
-			priority: heatLossOnPath,
-		})
-		// try to go up
-		heap.Push(pq, &Item{
-			value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: 1},
-			priority: heatLossOnPath,
-		})
-		if straightDistance < 3 {
-			// continue straight
+			// try to go up
 			heap.Push(pq, &Item{
-				value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+				value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: 1},
 				priority: heatLossOnPath,
 			})
-		}
-	case "up":
-		// try to go right
-		heap.Push(pq, &Item{
-			value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: 1},
-			priority: heatLossOnPath,
-		})
-		// try to go left
-		heap.Push(pq, &Item{
-			value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: 1},
-			priority: heatLossOnPath,
-		})
-		if straightDistance < 3 {
-			// continue straight
+			if straightDistance < 3 {
+				// continue straight
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+					priority: heatLossOnPath,
+				})
+			}
+		case "left":
+			// try to go down
 			heap.Push(pq, &Item{
-				value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+				value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: 1},
 				priority: heatLossOnPath,
 			})
-		}
-	case "down":
-		// try to go right
-		heap.Push(pq, &Item{
-			value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: 1},
-			priority: heatLossOnPath,
-		})
-		// try to go left
-		heap.Push(pq, &Item{
-			value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: 1},
-			priority: heatLossOnPath,
-		})
-		if straightDistance < 3 {
-			// continue straight
+			// try to go up
 			heap.Push(pq, &Item{
-				value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+				value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: 1},
 				priority: heatLossOnPath,
 			})
+			if straightDistance < 3 {
+				// continue straight
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+					priority: heatLossOnPath,
+				})
+			}
+		case "up":
+			// try to go right
+			heap.Push(pq, &Item{
+				value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+				priority: heatLossOnPath,
+			})
+			// try to go left
+			heap.Push(pq, &Item{
+				value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+				priority: heatLossOnPath,
+			})
+			if straightDistance < 3 {
+				// continue straight
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+					priority: heatLossOnPath,
+				})
+			}
+		case "down":
+			// try to go right
+			heap.Push(pq, &Item{
+				value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+				priority: heatLossOnPath,
+			})
+			// try to go left
+			heap.Push(pq, &Item{
+				value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+				priority: heatLossOnPath,
+			})
+			if straightDistance < 3 {
+				// continue straight
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+					priority: heatLossOnPath,
+				})
+			}
+		default:
+			panic(fmt.Sprintf("Unknown direction: %v", direction))
 		}
-	default:
-		panic(fmt.Sprintf("Unknown direction: %v", direction))
+	} else {
+		cannotTurnBeforeStraightDistance := 4
+		maxStraightDistance := 10
+		switch direction {
+		case "right":
+			if straightDistance >= cannotTurnBeforeStraightDistance {
+				// try to go down
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+					priority: heatLossOnPath,
+				})
+				// try to go up
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+					priority: heatLossOnPath,
+				})
+			}
+
+			if straightDistance < maxStraightDistance {
+				// continue straight
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+					priority: heatLossOnPath,
+				})
+			}
+		case "left":
+
+			if straightDistance >= cannotTurnBeforeStraightDistance {
+				// try to go down
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+					priority: heatLossOnPath,
+				})
+				// try to go up
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+					priority: heatLossOnPath,
+				})
+			}
+			if straightDistance < maxStraightDistance {
+				// continue straight
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+					priority: heatLossOnPath,
+				})
+			}
+		case "up":
+
+			if straightDistance >= cannotTurnBeforeStraightDistance {
+				// try to go right
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+					priority: heatLossOnPath,
+				})
+				// try to go left
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+					priority: heatLossOnPath,
+				})
+			}
+			if straightDistance < maxStraightDistance {
+				// continue straight
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX, fromY: fromY - 1, toX: toX, toY: toY, direction: "up", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+					priority: heatLossOnPath,
+				})
+			}
+		case "down":
+
+			if straightDistance >= cannotTurnBeforeStraightDistance {
+				// try to go right
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX + 1, fromY: fromY, toX: toX, toY: toY, direction: "right", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+					priority: heatLossOnPath,
+				})
+				// try to go left
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX - 1, fromY: fromY, toX: toX, toY: toY, direction: "left", currentHeatLoss: heatLossOnPath, straightDistance: 1},
+					priority: heatLossOnPath,
+				})
+			}
+			if straightDistance < maxStraightDistance {
+				// continue straight
+				heap.Push(pq, &Item{
+					value:    Path{fromX: fromX, fromY: fromY + 1, toX: toX, toY: toY, direction: "down", currentHeatLoss: heatLossOnPath, straightDistance: straightDistance + 1},
+					priority: heatLossOnPath,
+				})
+			}
+		default:
+			panic(fmt.Sprintf("Unknown direction: %v", direction))
+		}
 	}
+
 	if debug {
 		fmt.Printf("pq len: %v\n", pq.Len())
 	}
 	return -1
 }
 
-func partOne(lines []string) {
+func solve(lines []string) {
 	smallestHeatLoss := make([][]map[string]int, len(lines))
 	for i, line := range lines {
 		if debug {
@@ -221,11 +319,66 @@ func partOne(lines []string) {
 			lowestHeatLoss = heatLoss
 		}
 	}
+	if debug {
+		fmt.Println(smallestHeatLoss[len(lines)-1][len(lines[0])-1])
+		key := "right,1"
+		x := len(lines[0]) - 1
+		y := len(lines) - 1
+		for true {
+			if x < 0 || y < 0 || y >= len(lines) || x >= len(lines[y]) {
+				break
+			}
+			if (x == 0 && y == 0) || (x == 1 && y == 0) || (x == 0 && y == 1) {
+				break
+			}
+			value := smallestHeatLoss[y][x][key]
+			keySplit := strings.Split(key, ",")
+			direction := keySplit[0]
+			straightDistance, _ := strconv.Atoi(keySplit[1])
+			heatLoss, _ := strconv.Atoi(string(lines[y][x]))
+
+			//fmt.Printf("x: %v, y: %v, direction: %v, straightDistance: %v, heatLoss: %v, key: %v, value: %v\n", x, y, direction, straightDistance, heatLoss, key, value)
+
+			switch direction {
+			case "right":
+				lines[y] = lines[y][:x] + ">" + lines[y][x+1:]
+				x--
+			case "left":
+				lines[y] = lines[y][:x] + "<" + lines[y][x+1:]
+				x++
+			case "up":
+				lines[y] = lines[y][:x] + "^" + lines[y][x+1:]
+				y++
+			case "down":
+				lines[y] = lines[y][:x] + "v" + lines[y][x+1:]
+				y--
+			}
+			if straightDistance > 1 {
+				key = fmt.Sprintf("%v,%v", direction, straightDistance-1)
+
+				//fmt.Printf("Found key: %v : %v (%v)\n", key, value-heatLoss, smallestHeatLoss[y][x])
+			} else {
+				found := false
+				for k := range smallestHeatLoss[y][x] {
+					if value-heatLoss == smallestHeatLoss[y][x][k] {
+						found = true
+						key = k
+						//fmt.Printf("Found key: %v : %v (%v)\n", key, value-heatLoss, smallestHeatLoss[y][x])
+						break
+					}
+				}
+				if !found {
+					panic(fmt.Sprintf("Could not find key for value %v (%v)", value-heatLoss, smallestHeatLoss[y][x]))
+				}
+			}
+		}
+		for _, line := range lines {
+			fmt.Println(line)
+		}
+
+		fmt.Println(smallestHeatLoss[len(lines)-1][len(lines[0])-1])
+	}
 	fmt.Printf("Lowest heat loss: %v\n", lowestHeatLoss)
-}
-
-func partTwo(lines []string) {
-
 }
 
 func main() {
@@ -239,11 +392,7 @@ func main() {
 
 	startTime := time.Now()
 
-	if part == 1 {
-		partOne(lines)
-	} else {
-		partTwo(lines)
-	}
+	solve(lines)
 
 	endTime := time.Now()
 	duration := endTime.Sub(startTime)
