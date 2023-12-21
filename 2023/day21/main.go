266ca05f9d9d983e3d9cd7897b362c4e78946998
Reksa97 <reko.kalkaja@gmail.com>
2023-12-21 19:35:25 +0200

    2023 solve day 21 part 2

--- 2023/day21/main.go
+++ 2023/day21/main.go
@@ -31,6 +31,11 @@ type Coordinate struct {
 	y int
 }
 
+type MapShard struct {
+	x int
+	y int
+}
+
 func partOne(originalLines []string) {
 	steps := 64
 	lines, startX, startY := parseInput(originalLines)
@@ -86,8 +91,103 @@ func partOne(originalLines []string) {
 
 }
 
-func partTwo(lines []string) {
+func partTwoFindThreeValuesFromQuadraticFunction(originalLines []string) {
+	steps := 26501365
+	lines, startX, startY := parseInput(originalLines)
+	if debug {
+		fmt.Println(startX, startY)
+		for _, line := range lines {
+			fmt.Println(line)
+		}
+		for _, line := range originalLines {
+			fmt.Println(line)
+		}
+	}
 
+	possiblePlaces := make(map[MapShard]map[Coordinate]bool)
+	possiblePlaces[MapShard{0, 0}] = make(map[Coordinate]bool)
+	possiblePlaces[MapShard{0, 0}][Coordinate{startX, startY}] = true
+	startTime := time.Now()
+	possiblePlacesCounts := make([]int, 0)
+	for step := 0; step <= steps; step++ {
+		nextPlaces := make(map[MapShard]map[Coordinate]bool, 0)
+		totalPossiblePlacesCount := 0
+		for mapShard := range possiblePlaces {
+			for coordinate := range possiblePlaces[mapShard] {
+				totalPossiblePlacesCount++
+				if _, ok := nextPlaces[mapShard]; !ok {
+					nextPlaces[mapShard] = make(map[Coordinate]bool)
+				}
+				// South
+				if coordinate.y+1 < len(lines) && lines[coordinate.y+1][coordinate.x] == '.' {
+					nextPlaces[mapShard][Coordinate{coordinate.x, coordinate.y + 1}] = true
+				} else if coordinate.y+1 >= len(lines) && lines[0][coordinate.x] == '.' {
+					// Add new map shard if not already added
+					if _, ok := nextPlaces[MapShard{mapShard.x, mapShard.y + 1}]; !ok {
+						nextPlaces[MapShard{mapShard.x, mapShard.y + 1}] = make(map[Coordinate]bool)
+					}
+					nextPlaces[MapShard{mapShard.x, mapShard.y + 1}][Coordinate{coordinate.x, 0}] = true
+				}
+				// North
+				if coordinate.y-1 >= 0 && lines[coordinate.y-1][coordinate.x] == '.' {
+					nextPlaces[mapShard][Coordinate{coordinate.x, coordinate.y - 1}] = true
+				} else if coordinate.y-1 < 0 && lines[len(lines)-1][coordinate.x] == '.' {
+					// Add new map shard if not already added
+					if _, ok := nextPlaces[MapShard{mapShard.x, mapShard.y - 1}]; !ok {
+						nextPlaces[MapShard{mapShard.x, mapShard.y - 1}] = make(map[Coordinate]bool)
+					}
+					nextPlaces[MapShard{mapShard.x, mapShard.y - 1}][Coordinate{coordinate.x, len(lines) - 1}] = true
+				}
+				// East
+				if coordinate.x+1 < len(lines[coordinate.y]) && lines[coordinate.y][coordinate.x+1] == '.' {
+					nextPlaces[mapShard][Coordinate{coordinate.x + 1, coordinate.y}] = true
+				} else if coordinate.x+1 >= len(lines[coordinate.y]) && lines[coordinate.y][0] == '.' {
+					// Add new map shard if not already added
+					if _, ok := nextPlaces[MapShard{mapShard.x + 1, mapShard.y}]; !ok {
+						nextPlaces[MapShard{mapShard.x + 1, mapShard.y}] = make(map[Coordinate]bool)
+					}
+					nextPlaces[MapShard{mapShard.x + 1, mapShard.y}][Coordinate{0, coordinate.y}] = true
+				}
+				// West
+				if coordinate.x-1 >= 0 && lines[coordinate.y][coordinate.x-1] == '.' {
+					nextPlaces[mapShard][Coordinate{coordinate.x - 1, coordinate.y}] = true
+				} else if coordinate.x-1 < 0 && lines[coordinate.y][len(lines[coordinate.y])-1] == '.' {
+					// Add new map shard if not already added
+					if _, ok := nextPlaces[MapShard{mapShard.x - 1, mapShard.y}]; !ok {
+						nextPlaces[MapShard{mapShard.x - 1, mapShard.y}] = make(map[Coordinate]bool)
+					}
+					nextPlaces[MapShard{mapShard.x - 1, mapShard.y}][Coordinate{len(lines[coordinate.y]) - 1, coordinate.y}] = true
+				}
+			}
+		}
+		if step == (65 + 131*2) {
+			stepTime := time.Now().Sub(startTime)
+			timeUntilReady := int(stepTime.Microseconds()) * (steps - step)
+			if timeUntilReady > 10000000 {
+				fmt.Printf("Calculating step %v took %v ms, if the rest took as long, it would take %v minutes until answer was brute forced (actually time per step will grow exponentially so it would probably never finish)\n", step, stepTime.Milliseconds(), timeUntilReady/1000000/60)
+			}
+		}
+
+		// 65 steps to reach the border of the first map shard
+		// 131 steps from there to reach the border of the second map shard
+		if step == 65 || step == (65+131) || step == (65+131*2) {
+			possiblePlacesCounts = append(possiblePlacesCounts, totalPossiblePlacesCount)
+			if step == 327 {
+				break
+			}
+		}
+		possiblePlaces = nextPlaces
+		startTime = time.Now()
+	}
+	fmt.Println()
+	fmt.Println("Go to Wolfram Alpha and search for quadratic fit calculator")
+	fmt.Println("https://www.wolframalpha.com/input?i=quadratic+fit+calculator")
+	fmt.Println("data set of x values: {0, 1, 2}")
+	fmt.Printf("data set of y values: {%v, %v, %v}\n", possiblePlacesCounts[0], possiblePlacesCounts[1], possiblePlacesCounts[2])
+	// steps = 65 + 131 * x
+	// x = (steps - 65) / 131
+	useX := (steps - 65) / 131
+	fmt.Printf("Click 'Least-squares best fit' to open it and add ' where x = %v' at the end of the input\n\n", useX)
 }
 
 func main() {
@@ -104,7 +204,7 @@ func main() {
 	if part == 1 {
 		partOne(lines)
 	} else {
-		partTwo(lines)
+		partTwoFindThreeValuesFromQuadraticFunction(lines)
 	}
 
 	endTime := time.Now()
